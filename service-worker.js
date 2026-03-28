05b7e0a714338e1e2f45a6031b54f9f2f8bbb979
Jorge Sanhueza <jsanhueza@gmail.com>
2026-03-27 22:27:02 -0300

    rollback ala version anterior con fuentes desordenados, pero se ve bien

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,19 @@
-const CACHE_NAME = '2a-app-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/data/horario.json',
-  // agrega aquí tus JSON de asignaturas:
-  '/data/CalendarioPruebas.json',
-  '/data/FormacionCiudadana.json',
-  '/data/Math.json'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                'index.html',
+                'manifest.json',
+                'icon.png'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
